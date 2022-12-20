ad1a90b17e1b07008a5ade85c6ffe2033bc27ff4
xHK416x <wespritchard@gmail.com>
2022-12-20 17:10:49 +0000

    Lab 12 Complete

---
+++ lab_input/iptaker02.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   print() - concatenate vs print a series of items"""
+
+def main():
+
+    # collect string input from the user
+    user_input = input("Please enter an IPv4 IP address:")
+    
+    ## the line below creates a single string that is passed to print()
+    # print("You told me the IPv4 address is: " + user_input)
+    
+    ## print() can be given a series of objects separated by a comma
+    print("You told me the IPv4 address is:", user_input)
+
+    # asking user for 'vendor name'
+    vendor = input("Please input the vendor name: ")
+    print(vendor)
+
+main()
