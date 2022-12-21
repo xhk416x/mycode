6b91ad6588e18e6caa79cf4bb938ebe981b0156e
xHK416x <wespritchard@gmail.com>
2022-12-21 20:08:11 +0000

    challenge 17

---
+++ challenges/challenge17.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+mylist= [1,"2",3,4,5, "Python"]
+
+name= input("What is your name?\n")
+
+# This is what you should see when print runs-
+# Hi <name>! Welcome to Day 2 of Python Training!
+
+name= name.capitalize()
+
+print("Hi " + name + "! Welcome to Day " + mylist[1] + " of " + mylist[5] + " Training!")
