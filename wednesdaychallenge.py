ab5547ef0ad47d8151cfc38737c60603474af712
xhk416x <68830971+xhk416x@users.noreply.github.com>
2023-01-04 11:26:06 -0500

    updated to include import protection and main function

--- wednesdaychallenge.py
+++ wednesdaychallenge.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 
-number= 2
+def main():
+    number= 2
 
-name= input("What is your name?\n")
+    name= input("What is your name?\n")
 
 # This is what you should see when print runs-
 # Hi <name>! Welcome to Day 2 of Python Training!
 
-name= name.capitalize()
+    name= name.capitalize()
 
 #edited to use f-string
 #print("Hi " + name + "! Welcome to Day " + mylist[1] + " of " + mylist[5] + " Training!")
-print(f"Hi {name}! Welcome to Day {number} of Python Training!")
+    print(f"Hi {name}! Welcome to Day {number} of Python Training!")
 
 #import protection. Will not run main() unless file is run directly
 if __name__ == "__main__":
