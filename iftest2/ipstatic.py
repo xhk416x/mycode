ac9acff42c219f546a8c692cf1684bb4cc526af5
xHK416x <wespritchard@gmail.com>
2023-01-04 23:12:40 +0000

    working on if, elif, else logic

---
+++ iftest2/ipstatic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   Conditionals - strings test true"""
+
+ipchk = "192.168.0.1"
+
+# a string tests as True
+if ipchk:
+   print("Looks like the IP address was set: " + ipchk)
