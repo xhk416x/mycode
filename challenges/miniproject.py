c6f408b064e6f7afb359dc5157d5e89a47de4da3
xHK416x <wespritchard@gmail.com>
2023-01-05 17:12:22 +0000

    finished disney dictionaries

--- challenges/miniproject.py
+++ challenges/miniproject.py
@@ -1,22 +1,28 @@
 whyq= {
-        "question" : "Why are you going to disney?"
+        "question" : "Why are you going to disney?",
         "Rides" : "hollywood_studios",
         "Characters": "magic_kingdom",
         "Food" : "epcot",
         "Not sure???" : "animal_kingdom"
     }
 dressupq= {
-            "question" : "If you had to dress up as a Disney character, which of the following most applies to you?",
-            "Pixar Character" : "hollywood_studios",
-            "Prince/Princess": "magic_kingdom",
-            "What are you talking about? I just came here for the food." : "epcot",
-            "A Navi (Avatar)" : "animal_kingdom"
+        "question" : "If you had to dress up as a Disney character, which of the following most applies to you?",
+        "Pixar Character" : "hollywood_studios",
+        "Prince/Princess": "magic_kingdom",
+        "What are you talking about? I just came here for the food." : "epcot",
+        "A Navi (Avatar)" : "animal_kingdom"
 }
-
 souvenirq= {
-        "question" : "What souvenir are you most likely to buy?"
+        "question" : "What souvenir are you most likely to buy?",
         "LightSaber" : "hollywood_studios",
         "Mickey Ears": "magic_kingdom",
-        "" : "epcot",
-        "" : "animal_kingdom"
+        "PINS! Time to get trading!" : "epcot",
+        "Stuffed Animal" : "animal_kingdom"
+}
+rideq= {
+        "question" : "Which ride are you most excited to ride?",
+        "Rise of the Resistance" : "hollywood_studios",
+        "Space Mountain": "magic_kingdom",
+        "Frozen Ever After" : "epcot",
+        "Flight of Passage" : "animal_kingdom"
 }
