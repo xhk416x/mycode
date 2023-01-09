a89c8689f119b66521ebccc2d58151cfe1cbcbf2
xHK416x <wespritchard@gmail.com>
2023-01-09 16:36:15 +0000

    updated script with "No more" line

--- challenges/99bottles.py
+++ challenges/99bottles.py
@@ -8,10 +8,14 @@ try:
     if bottles > 100:
         print("Whoa there, you don't have that many bottles on your wall.")
     elif bottles > 0 < 100:
-        for count in range(-1,bottles):
+        for count in range(0,bottles):
             print(f"{bottles} of beer on the wall!! {bottles} of beeeeeer!")
             print(f"Take one down, pass it around. {bottles} of beer on the wall.")
-            bottles -= 1           
+            bottles -= 1
+            time.sleep(1)
+        if bottles == 0:
+            print("No more bottles of beer on the wall! No more bottles of beeeeer!")
+            print("Now I'm sad, and probably an alcoholic. No more bottles of beer on the wall.")           
 except:
     print(f"That wasn't a number! I can't count down from {bottles}")
 
