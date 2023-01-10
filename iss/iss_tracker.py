6016e7441d6e0e8cb0c9369aa7885da614b5c8da
xHK416x <wespritchard@gmail.com>
2023-01-10 16:43:48 +0000

    updated iss_tracker variable locations

--- iss/iss_tracker.py
+++ iss/iss_tracker.py
@@ -7,12 +7,12 @@ import time
 import reverse_geocoder
 
 iss_local = "http://api.open-notify.org/iss-now.json"
+iss_gps= (response["iss_position"]["latitude"],response["iss_position"]["longitude"])
+response= requests.get(iss_local).json()
 
 def main():
-    response= requests.get(iss_local).json()
     print("CURRENT LOCATION OF THE ISS:")
     print(time.strftime('%m-%d-%Y %H:%M:%S', time.localtime()))
-    iss_gps= (response["iss_position"]["latitude"],response["iss_position"]["longitude"])
     print("Long: ", response["iss_position"]["longitude"])
     print("Lat: ", response["iss_position"]["latitude"])
     print("City, State/Province, Country: ", reverse_geocoder.search(iss_gps)[0]["name"], ", ", reverse_geocoder.search(iss_gps)[0]["admin1"], ", ", reverse_geocoder.search(iss_gps)[0]["cc"], sep="")
