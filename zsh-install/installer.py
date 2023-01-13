2eeaa78b802cbdd5f8b6d8c35b074acb6217cf31
xhk416x <wespritchard@gmail.com>
2023-01-13 19:19:19 +0000

    trying to eliminate bash

--- zsh-install/installer.py
+++ zsh-install/installer.py
@@ -12,6 +12,8 @@ import os
 ## pull in env variables
 HOME = os.getenv('HOME')
 PWD = os.getcwd()
+#### install prereqs
+os.system("sudo apt update && sudo apt install python3-pip ansible -y")
 ## below will download all pip modules in dependencies.txt
 import py_mod_deps
 ## after py_mod_deps installs modules, this will import them  
