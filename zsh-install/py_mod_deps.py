4152c6c22fa0a390c70569ee21c78b0ca6d29248
xhk416x <wespritchard@gmail.com>
2023-01-13 20:50:56 +0000

    trying new internet code

--- zsh-install/py_mod_deps.py
+++ zsh-install/py_mod_deps.py
@@ -1,28 +1,20 @@
 # #!/usr/bin/env python3
 # """This script installs 3rd party pip modules"""
 
-import pip
+# import pip
 import importlib
-
+def install_file(package: str):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--no-cache-dir", "--no-index", "--find-links", "whlFolder", package], shell=True)
 def import_with_auto_install(package):
     try:
         return importlib.import_module(package)
         print("try")
     except ImportError:
-        pip.main(['install', package])
-        importlib.invalidate_caches()
+        # pip.main(['install', package])
+        install_file(package)
         print("except")
-    print("pre-return")
-    return importlib.import_module(package)
-    
-    # with open("dependencies.txt", "r") as foo:
-    #     mods = []
-    #     for line in foo:
-    #         modname= line.strip("\n")
-    #         mods.append(modname)
-    #         modname= import_with_auto_install(modname)
-    #     for mod in mods:
-    #         globals()[mod] = importlib.import_module(mod)
+    # print("pre-return")
+    # return importlib.import_module(package)
 
 if __name__ == '__main__':
     with open("dependencies.txt", "r") as foo:
@@ -34,5 +26,3 @@ if __name__ == '__main__':
         print(mods)
         for mod in mods:
             globals()[mod] = importlib.import_module(mod)
-        print(globals())
-        ansible_runner.run()
